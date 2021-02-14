1619604e8a327f68fa919de8beba4076a19baf32
JamesCostello-dev <jcotab@gmail.com>
2021-02-14 13:08:19 -0700

    user-routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,1 +1,18 @@
 // user routes
+const router = require('express').Router();
+
+// get all users
+// get user by id populate thought and friend data
+// post new user
+// put update user by id
+// remove user by id
+
+const { getAllUser, getUserById, createUser, updateUser, deleteUser } = require('../../controllers/user-controllers');
+
+// api/users
+router.route('./').get(getAllUser).post(createUser);
+
+// api/users/:id
+router.route('./:id').get(getUserById).put(updateUser).delete(deleteUser);
+
+module.exports = router;
