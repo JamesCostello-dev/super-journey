b1f5daf43cb5623f5d7b61c52d30f5664a84d4b0
JamesCostello-dev <jcotab@gmail.com>
2021-02-14 13:35:11 -0700

    index routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,10 @@
 // index api
 const router = require('express').Router();
 
-const userRoute = require('./user-routes');
+const userRoutes = require('./user-routes');
 const thoughtRoutes = require('./thought-routes');
 
-router.use('./users', userRoute);
-router.use('./thoughts', thoughtRoutes);
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
